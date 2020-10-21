24a4cd2f7a1baa4441f05197a69c514fec1989fe
Vincent V <51407346+indronna@users.noreply.github.com>
2020-10-21 08:43:02 +0200

    Ajout d'un player controller

---
+++ DanmaDoom/Assets/PlayerMouvement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouvement : MonoBehaviour
+{
+
+    // https://youtu.be/_QajrabyTJc?t=1098 
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    Vector3 velocity;
+    public Transform groundCheck;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
